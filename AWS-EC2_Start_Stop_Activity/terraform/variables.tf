333052095fc3e14cc04b75408754e13473945d1c
MayurSabade <79620475+mayur0319@users.noreply.github.com>
2024-07-01 15:36:38 +0530

    Update variables.tf

--- AWS-EC2_Start_Stop_Activity/terraform/variables.tf
+++ AWS-EC2_Start_Stop_Activity/terraform/variables.tf
@@ -9,7 +9,7 @@ variable "environment" {
 variable "resource_prefix" {
   type        = string
   description = "Resource Prefix"
-  default     = "sni-p01-util-instance-scheduler"
+  default     = ""
   
 }
 
@@ -28,11 +28,11 @@ variable "account_tags" {
 variable "exclude_account_list" {
   type        = list(string)
   description = "Exclude Accounts list"
-  default     = ["905162661662","655460373928","633910205101","075805711779","125487031927","463620995861","075869202828","799352561952"]
+  default     = []
 }
 
 variable "organization_account" {
   type        = string
   description = "Organisation Account ID"
-  default     = "075869202828"
+  default     = ""
 }
